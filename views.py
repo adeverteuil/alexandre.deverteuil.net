c95040e032ab1ef719b3eea035851700b4ae6404
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-08-22 16:36:26 -0400

    Added basic urls.py, admin.py, templates.

--- views.py
+++ views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from pages.models import Page
+
+
+class DetailView(generic.DetailView):
+    model = Page
