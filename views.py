e97e88d3ddffeca62bbab25caf21ae5afa19b509
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-08-23 21:54:40 -0400

    Removed 5 post limit for the index.

--- views.py
+++ views.py
@@ -11,7 +11,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published posts."""
-        return Post.objects.order_by("-pub_date")[:5]
+        return Post.objects.order_by("-pub_date")
 
 
 class DetailView(generic.DetailView):
