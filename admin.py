ef2313d3b2689c25b3b613bfd9a076477f5d7eb0
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-08-25 00:00:24 -0400

    Added slugs to the Page model.

--- admin.py
+++ admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from pages.models import Page
 
 class PageAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ("title", "slug", "public")
+    prepopulated_fields = {"slug": ("title",)}
+
 
 admin.site.register(Page, PageAdmin)
--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Page(models.Model):
     
     title = models.CharField(max_length=64)
+    slug = models.SlugField()
     body = models.TextField()
     public = models.BooleanField(default=False)
     description = models.TextField()
