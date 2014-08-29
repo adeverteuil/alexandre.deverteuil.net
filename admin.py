a5d215711f686c8a75ee60371dcbe5afffef4cfa
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-08-29 16:25:52 -0400

    Created admin actions for bulk publishing posts.

--- admin.py
+++ admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from blog.models import Post, Category
 
 
+def make_public(modeladmin, request, queryset):
+    queryset.update(public=True)
+make_public.short_description = "Mark as public"
+
+def make_not_public(modeladmin, request, queryset):
+    queryset.update(public=False)
+make_not_public.short_description = "Mark as private"
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "summary", "pub_date", "public")
     prepopulated_fields = {"slug": ("title",)}
+    actions = [make_public, make_not_public]
 
 
 admin.site.register(Post, PostAdmin)
