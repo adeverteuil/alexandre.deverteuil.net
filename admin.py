c7fe71b8038a864c409b4d1b0391dcf3ba8e6450
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-09-02 15:18:02 -0400

    Represent dates in yyyy-mm-dd hh:mm format on the admin list display.

--- admin.py
+++ admin.py
@@ -13,13 +13,20 @@ make_not_public.short_description = "Mark as private"
 
 class PostAdmin(admin.ModelAdmin):
 
-    list_display = ("title", "summary", "pub_date", "public")
+    list_display = ("title", "summary", "format_date", "public")
     list_editable = ("summary", "public")
     fields = (("title", "pub_date"), ("slug", "public"), "summary", "body")
     ordering = ("-pub_date",)
     prepopulated_fields = {"slug": ("title",)}
     actions = [make_public, make_not_public]
     date_hierarchy = "pub_date"
+
+    # Represent dates in yyyy-mm-dd hh:mm format on the admin list display.
+    # source:  https://docs.djangoproject.com/en/1.6/topics/i18n/
+    def format_date(self, obj):
+        return obj.pub_date.strftime("%Y-%m-%d %H:%M")
+    format_date.short_description = "Date published"
+    format_date.admin_order_field = "pub_date"
 
     class Media:
         css = {
