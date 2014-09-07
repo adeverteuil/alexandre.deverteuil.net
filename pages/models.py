5217702cd0fb53053770b8345dfc88663d862807
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-09-07 10:35:34 -0400

    Added the pub_date field in pages app.

--- pages/models.py
+++ pages/models.py
@@ -4,3 +4,4 @@ from django.contrib.flatpages.models import FlatPage
 class Page(FlatPage):
     
     description = models.CharField(max_length=256)
+    pub_date = models.DateTimeField(blank=True, null=True)
