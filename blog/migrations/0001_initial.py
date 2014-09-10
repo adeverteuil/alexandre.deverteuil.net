30b11af5e93e89f449b4116dbfa3f1be99e71ace
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-09-09 23:23:52 -0400

    Upgrade to django 1.7 and remove south.

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -1,38 +1,40 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Post'
-        db.create_table('blog_post', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
-            ('body', self.gf('django.db.models.fields.TextField')()),
-            ('public', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('blog', ['Post'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Post'
-        db.delete_table('blog_post')
-
-
-    models = {
-        'blog.post': {
-            'Meta': {'object_name': 'Post'},
-            'body': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
-            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
-        }
-    }
-
-    complete_apps = ['blog']
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('name', models.CharField(unique=True, max_length=64)),
+            ],
+            options={
+                'verbose_name_plural': 'categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=256)),
+                ('slug', models.SlugField()),
+                ('pub_date', models.DateTimeField(verbose_name='date published')),
+                ('body', models.TextField()),
+                ('summary', models.CharField(null=True, blank=True, max_length=256)),
+                ('public', models.BooleanField(default=False)),
+                ('categories', models.ManyToManyField(null=True, blank=True, to='blog.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
