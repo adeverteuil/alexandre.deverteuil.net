71bc44a07d4ab2595cc6dbbe403da43830545051
Alexandre de Verteuil <claudelepoisson@gmail.com>
2014-09-18 15:46:39 -0400

    Added RSS feed icon on blog footer.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog import views, feeds
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^rss/$', feeds.LatestEntriesFeed()),
+    url(r'^rss/$', feeds.LatestEntriesFeed(), name="rss"),
     url(
         r'^(?P<slug>[-_a-z0-9]+)/$',
         views.DetailView.as_view(),
